40a165bfd6675f407275908f490a299ad5f3f0ce
pandusiregar <pandu.siregar@mokapos.com>
2020-02-03 11:58:33 +0700

    remove checking docker status

--- ToolsInstallation/DockerInstallationUbuntu.sh
+++ ToolsInstallation/DockerInstallationUbuntu.sh
@@ -14,9 +14,6 @@ sudo apt install docker-ce -y
 
 sudo systemctl start docker
 
-sudo systemctl status docker
-
-
 
 #Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
